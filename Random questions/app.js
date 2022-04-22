d9aba4ba44b2e79054828273f4c596e5650eb2a3
Öryan Karakaş <63560783+oryan9@users.noreply.github.com>
2022-04-22 13:47:02 +0300

    Random question - javascript
    javascript project

---
+++ Random questions/app.js
@@ -0,0 +1,37 @@
+//using selectors inside the element
+// traversing the dom
+
+
+
+
+// const questions = document.querySelectorAll('.question')
+
+// question.forEach(function(question){
+
+//     const btn = question.querySelector('.question-btn')
+    
+//     btn.addEventListener('click',function(){
+
+//         question.forEach(function(item){
+
+//             if(item !== question){
+//                 item.classList.remove('show-text')
+//             }
+//         })
+//         question.classList.toggle('show-text')
+//     })
+
+// })
+
+
+
+
+
+const btns = document.querySelectorAll('.question-btn')
+
+btns.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+        const question = e.currentTarget.parentElement.parentElement
+        question.classList.toggle('show-text')
+    })
+})
